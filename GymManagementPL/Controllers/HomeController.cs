e16b55730a1a8f0eb9165ee77225a090166d72cc
malakmedhat1 <malakmedhat41@gmail.com>
2025-10-19 18:42:45 +0300

    Session04

--- GymManagementPL/Controllers/HomeController.cs
+++ GymManagementPL/Controllers/HomeController.cs
@@ -1,32 +1,15 @@
-using System.Diagnostics;
-using GymManagementPL.Models;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace GymManagementPL.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
+        // Actions
+        // Base URL /Home/Index
+        // [NoAction]
+        public ViewResult Index()
         {
             return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
